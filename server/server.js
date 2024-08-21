7b0cc52829c3749ee9d0a1748361d57da5e68325
Mahdi Haidary <m.mahdi.haidary.786@gmail.com>
2024-08-21 07:44:17 +0200

    updated server js

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const http = require('http');
-const socketIo = require('socket.io');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const http = require("http");
+const socketIo = require("socket.io");
 
 // Routes
-const userRoute = require("./routers/userRoutes")
-const quizzRoute = require("./routers/quizRoutes")
-
-
+const userRoute = require("./routers/userRoutes");
+const quizzRoute = require("./routers/quizRoutes");
 
 const app = express();
 const server = http.createServer(app);
@@ -19,32 +17,19 @@ const io = socketIo(server);
 
 const PORT = 5000;
 
+mongoose
+  .connect(
+    "mongodb+srv://oula:94yrEyVqKA0V1k3g@firstcluster.woitwky.mongodb.net/quizapp"
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
+app.use("/user", userRoute);
+app.use("/quizz", quizzRoute);
 
-mongoose.connect("mongodb+srv://oula:94yrEyVqKA0V1k3g@firstcluster.woitwky.mongodb.net/quizapp")
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log(err));
-
-
-
-
-app.use("/user", userRoute)
-app.use("/quizz", quizzRoute)
-
-
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
-
-
-
-
-
-
-
-
-
-
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
